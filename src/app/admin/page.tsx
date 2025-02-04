e3ce9df4999930c63b6482ddb906ae54d7223d3d
vayaji <176355251+vayaji@users.noreply.github.com>
2025-02-05 00:18:01 +0900

    Add icon

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,3 +1,5 @@
+import AddDrink from "@/components/icons/add-drink";
+import Drink from "@/components/icons/drink";
 import Title from "@/components/title";
 import Link from "next/link";
 import React from "react";
@@ -6,18 +8,20 @@ export default function Admin() {
   return (
     <>
       <Title title="トップ" />
-      <div className="flex justify-evenly gap-8 max-w-screen-lg mx-auto p-4 h-[calc(100vh-130px)]">
+      <div className="flex justify-center gap-8 max-w-screen-lg mx-auto p-4 h-[calc(100vh-130px)]">
         <Link
           href="/admin/drink"
-          className="text-textColor text-button flex-1 border-2 border-accentBaseColor bg-accentLightColor rounded-xl flex flex-col justify-center items-center h-min aspect-square"
+          className="text-textColor text-button flex-1 border-4 border-accentDarkColor bg-baseColor rounded-xl flex flex-col justify-center items-center h-min aspect-square max-w-64"
         >
-          商品一覧
+          <Drink />
+          ドリンク一覧
         </Link>
         <Link
           href="/admin/drink/store"
-          className="text-textColor text-button flex-1 border-2 border-accentBaseColor bg-accentLightColor rounded-xl flex flex-col justify-center items-center h-min aspect-square"
+          className="text-textColor text-button flex-1 border-4 border-accentDarkColor bg-baseColor rounded-xl flex flex-col justify-center items-center h-min aspect-square max-w-64"
         >
-          商品追加
+          <AddDrink />
+          ドリンク追加
         </Link>
       </div>
     </>
